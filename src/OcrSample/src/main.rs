7b3f8171dc08a2f45fac52ce5b5290eb473f19e3
Michael Kokonowskyj <mikokono@microsoft.com>
2022-09-12 15:12:57 +0200

    use filename arg

--- src/OcrSample/src/main.rs
+++ src/OcrSample/src/main.rs
@@ -4,11 +4,16 @@ extern crate stopwatch;
 use stopwatch::{Stopwatch};
 use leptess::{leptonica, tesseract};
 use std::path::Path;
+use std::env;
 
 fn main() {
     let mut api = tesseract::TessApi::new(None, "eng").unwrap();
 
-    let pix = leptonica::pix_read(Path::new("/mnt/d/temp/di.png")).unwrap();
+    let args:Vec<String> = env::args().collect();
+    let filepath = &args[1];
+
+    let pix = leptonica::pix_read(Path::new(filepath)).unwrap();
+
     api.set_image(&pix);
     let sw = Stopwatch::start_new();
     let text = api.get_utf8_text();
